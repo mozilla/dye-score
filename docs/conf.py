06a0f4bcdeef527312a0cb6b39cf499da3016d5c
Sarah Bird <birdsarah@mozilla.com>
2019-04-01 02:10:43 -0500

    Mock out modules for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,16 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import sphinx_rtd_theme
 import dye_score
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['numpy', 'pandas', 'dask', 'xarray', 'bokeh',]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ---------------------------------------------
 
