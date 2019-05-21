316869b43547c4c67a2a0dc1a0cd4a84108b0dce
Sarah Bird <birdsarah@mozilla.com>
2019-05-21 14:30:58 -0500

    Add initial test for compute_distances

--- tests/test_dye_score.py
+++ tests/test_dye_score.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import dask.dataframe as dd
+import numpy as np
 import os
 import pandas as pd
 import pytest
+import xarray as xr
 import yaml
 
+from scipy.spatial.distance import chebyshev
 from pyarrow.lib import ArrowIOError
 from yaml.scanner import ScannerError
 
 from dye_score import DyeScore
+
+
+def write_config_file(tmpdir, sample_config):
+    config_file = os.path.join(tmpdir, 'config.yaml')
+    with open(config_file, 'w') as f:
+        f.write(yaml.dump(sample_config))
+    return config_file
+
+
+def daskify(df, npartitions=2):
+    # Turn pandas df into dask
+    return dd.from_pandas(df, npartitions=2)
 
 
 ##
@@ -18,13 +33,11 @@ from dye_score import DyeScore
 
 def test_data_validation_with_invalid_file(tmpdir, sample_config):
     # Set-up invalid data file and save config
-    data_dir = os.path.join(tmpdir, 'data.csv')
-    config_file = os.path.join(tmpdir, 'config.yaml')
+    data_file = os.path.join(tmpdir, 'data.csv')
     df = pd.DataFrame({'a': [1, 2, 3]})
-    df.to_csv(data_dir)
-    sample_config['INPUT_PARQUET_LOCATION'] = data_dir
-    with open(config_file, 'w') as f:
-        f.write(yaml.dump(sample_config))
+    df.to_csv(data_file)
+    sample_config['INPUT_PARQUET_LOCATION'] = data_file
+    config_file = write_config_file(tmpdir, sample_config)
     ds = DyeScore(config_file)
     # Test
     with pytest.raises(ArrowIOError):
@@ -33,20 +46,16 @@ def test_data_validation_with_invalid_file(tmpdir, sample_config):
 
 def test_data_validation_with_valid_file(tmpdir, sample_config):
     # Set-up valid data file and save config
-    data_dir = os.path.join(tmpdir, 'data.parquet')
-    config_file = os.path.join(tmpdir, 'config.yaml')
-    df = pd.DataFrame({
+    data_file = os.path.join(tmpdir, 'data.parquet')
+    daskify(pd.DataFrame({
         'top_level_url': ['a', 'b'],
         'document_url': ['a', 'b'],
         'script_url': ['c', 'd'],
         'symbol': ['e', 'f'],
         'func_name': ['g', 'h']
-    })
-    dfd = dd.from_pandas(df, npartitions=2)
-    dfd.to_parquet(data_dir)
-    sample_config['INPUT_PARQUET_LOCATION'] = data_dir
-    with open(config_file, 'w') as f:
-        f.write(yaml.dump(sample_config))
+    })).to_parquet(data_file)
+    sample_config['INPUT_PARQUET_LOCATION'] = data_file
+    config_file = write_config_file(tmpdir, sample_config)
     ds = DyeScore(config_file)
     # Test
     assert ds.validate_input_data() is True
@@ -83,3 +92,64 @@ def test_config_with_use_aws_true(asset_dir):
 def test_config_with_use_aws_false(asset_dir):
     ds = DyeScore(os.path.join(asset_dir, 'valid_config_aws_false.yaml'))
     assert ds.config('USE_AWS') is False
+
+
+###
+# Test compute_distances_for_dye_snippets
+###
+
+
+def test_standard_chebyshev_call_works(tmpdir, sample_config):
+    sample_config['DYESCORE_DATA_DIR'] = tmpdir.strpath
+    ds = DyeScore(write_config_file(tmpdir, sample_config))
+
+    random_array = np.random.rand(5, 2)
+    snippet_ids = ['0', '1', '2', '3', '4']  # 0 index for sanity :D
+    data = xr.DataArray(
+        random_array,
+        coords={
+            'snippet': snippet_ids,
+            'symbol': ['window.navigator', 'canvas.context'],
+        },
+        dims=('snippet', 'symbol')
+    )
+    f = ds.dye_score_data_file('snippets')
+    data.to_dataset(name='data').to_zarr(store=ds.get_zarr_store(f))
+
+    # Run Test
+    dye_snippets = ['2']
+    result_file = ds.compute_distances_for_dye_snippets(
+        dye_snippets, override=True
+    )
+
+    # Check Results
+    results = xr.open_zarr(store=ds.get_zarr_store(result_file))['data']
+    assert results.shape == (5, 1)
+    for s in snippet_ids:
+        actual_result = results.sel(snippet=s, dye_snippet='2').values
+        expected_result = chebyshev(random_array[2], random_array[int(s)])
+        assert actual_result == expected_result
+
+
+def test_passing_unavailable_string_fails(tmpdir, sample_config):
+    # Setup
+    sample_config['DYESCORE_DATA_DIR'] = tmpdir.strpath
+    ds = DyeScore(write_config_file(tmpdir, sample_config))
+    random_array = np.random.rand(5, 2)
+    snippet_ids = ['0', '1', '2', '3', '4']
+    data = xr.DataArray(
+        random_array,
+        coords={
+            'snippet': snippet_ids,
+            'symbol': ['window.navigator', 'canvas.context'],
+        },
+        dims=('snippet', 'symbol')
+    )
+    f = ds.dye_score_data_file('snippets')
+    data.to_dataset(name='data').to_zarr(store=ds.get_zarr_store(f))
+
+    # Run Test
+    with pytest.raises(KeyError):
+        ds.compute_distances_for_dye_snippets(
+            ['2'], override=True, distance_function='euclidean',
+        )
