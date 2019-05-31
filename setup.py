7b11c06b914f09a610906f9f9770af4e0b8fb48c
Sarah Bird <birdsarah@mozilla.com>
2019-05-31 14:00:08 -0500

    Bump version: 0.11.1 → 0.11.2

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mozilla/dye-score',
-    version='0.11.1',
+    version='0.11.2',
     zip_safe=False,
 )
